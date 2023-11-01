4d1b08b24d0ec88c23e524f201c2e1afd6a889c9
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-11-01 21:18:41 +0900

    feat: Pul 솔루션 따라 작성해보기

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,15 +1,9 @@
 defmodule NameBadge do
-  @default_department "OWNER"
-  def print(nil, name, nil) do
-      "#{name} - #{@default_department}"
-  end
-  def print(id, name, nil) do
-      "[#{id}] - #{name} - #{@default_department}"
-  end
-  def print(nil, name, department) do
-      "#{name} - #{String.upcase(department)}"
-  end
   def print(id, name, department) do
-      "[#{id}] - #{name} - #{String.upcase(department)}"
+    "#{
+      if id, do: "[#{id}] - ", else: ""
+    }#{name} - #{
+      if department, do: "#{String.upcase(department)}", else: "OWNER"
+    }"
   end
 end
