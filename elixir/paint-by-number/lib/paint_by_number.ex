dd3bc0bd21e62d95613484655800a7c5b7cff412
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-11-12 22:52:51 +0900

    feat: paint by number 제출

--- elixir/paint-by-number/lib/paint_by_number.ex
+++ elixir/paint-by-number/lib/paint_by_number.ex
@@ -16,19 +16,30 @@ defmodule PaintByNumber do
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
-    <<pixel_color_index::bitstring, picture::bitstring>>
+    <<pixel_color_index::size(palette_bit_size(color_count)), picture::bitstring>>
   end
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    if picture === "" do
+      nil
+    else
+      bit_size = palette_bit_size(color_count)
+      <<result::size(bit_size), _::bitstring>> = picture
+      result
+    end
   end
 
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    if picture === "" do
+      picture
+    else
+      bit_size = palette_bit_size(color_count)
+      <<_::size(bit_size), rest::bitstring>> = picture
+      rest
+    end
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
   end
 end
