35fa3883c72ddf38bc0b768a6bfbbcf1b8c926f0
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-11-17 00:14:00 +0900

    feat: library_fees 진행중

--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -8,18 +8,38 @@ defmodule LibraryFees do
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    checked_out_before_noon? = before_noon?(checkout_datetime)
+
+    if checked_out_before_noon? do
+      Date.add(checkout_datetime, 28)
+    else
+      Date.add(checkout_datetime, 29)
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    max(Date.diff(actual_return_datetime, planned_return_date), 0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    Date.day_of_week(datetime) === 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    {_, checkout_datetime, _} = DateTime.from_iso8601(checkout)
+    {_, return_datetime, _} = DateTime.from_iso8601(return)
+    planned_return_date = return_date(checkout_datetime)
+
+    late_days = days_late(planned_return_date, return_datetime)
+    is_monday? = monday?(return_datetime)
+    is_before_noon? = before_noon?(checkout_datetime)
+
+    case {is_monday?, is_before_noon?} do
+      {true, true} -> if late_days > 28 do trunc((late_days - 28) * rate * 0.5) end
+      {false, true} -> if late_days > 28 do (late_days - 28) * rate end
+      {true, false} -> if late_days > 29 do trunc((late_days - 29) * rate * 0.5) end
+      {false, false} -> if late_days > 29 do (late_days - 29) * rate end
+      _ -> 0
+    end
   end
 end
