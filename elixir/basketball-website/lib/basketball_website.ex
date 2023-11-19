004589d4d743fe530eec807d2088811a7c429670
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-11-19 20:18:19 +0900

    feat: basketball_website 세번째 제출, 커뮤니티 참고

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,21 +1,13 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    do_extract_from_path(data, path)
+    do_extract_from_path(data, String.split(path, "."))
   end
-  defp do_extract_from_path(data, path) do
-    if String.contains?(path, ".") do
-      [first, rest] = String.split(path, ".", parts: 2)
-      do_extract_from_path(data[first], rest)
-    else
-      data[path]
-    end
+  defp do_extract_from_path(data, []) do data end
+  defp do_extract_from_path(data, [first | rest]) do
+    do_extract_from_path(data[first], rest)
   end
 
   def get_in_path(data, path) do
-    if String.contains?(path, ".") do
-      get_in(data, String.split(path, "."))
-    else
-      get_in(data, [path])
-    end
+    get_in(data, String.split(path, "."))
   end
 end
