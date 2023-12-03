45d64dd6be1fce5eca553602dc728c038494a72d
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-12-03 22:42:06 +0900

    feat: file sniffer 완료

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -2,7 +2,7 @@ defmodule FileSniffer do
   def type_from_extension(extension) do
     case extension do
       "exe" -> "application/octet-stream"
-      "bmg" -> "image/bmp"
+      "bmp" -> "image/bmp"
       "png" -> "image/png"
       "jpg" -> "image/jpg"
       "gif" -> "image/gif"
@@ -10,31 +10,33 @@ defmodule FileSniffer do
     end
   end
 
-  def type_from_binary(file_binary) do
-    <<first_byte::binary-size(8), rest_file_binary::binary>> = file_binary
-    case first_byte do
-      <<0x7F>> -> do_type_from_binary(rest_file_binary, [<<0x45, 0x4C, 0x46>>], type_from_extension("exe"))
-      <<0x42>> -> do_type_from_binary(rest_file_binary, [<<0x4D>>], type_from_extension("bmp"))
-      <<0x89>> -> do_type_from_binary(rest_file_binary, [<<0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>], type_from_extension("png"))
-      <<0xFF>> -> do_type_from_binary(rest_file_binary, [<<0xD8, 0xFF>>], type_from_extension("jpg"))
-      <<0x47>> -> do_type_from_binary(rest_file_binary, [<<0x49, 0x46>>], type_from_extension("gif"))
-      _ -> nil
-    end
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>) do
+    type_from_extension("exe")
   end
-  defp do_type_from_binary(file_binary, remain_signature, media_type) do
-    if length(remain_signature) === 0 do
-      media_type
-    else
-      <<first_byte::binary-size(8), rest_file_binary::binary>> = file_binary
-      [match_signature | rest_signature] = remain_signature
-
-      if first_byte === match_signature do
-        do_type_from_binary(rest_file_binary, rest_signature, media_type)
-      end
-    end
+  def type_from_binary(<<0x42, 0x4D, _::binary>>) do
+    type_from_extension("bmp")
+  end
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>) do
+    type_from_extension("png")
+  end
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>) do
+    type_from_extension("jpg")
+  end
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>) do
+    type_from_extension("gif")
+  end
+  def type_from_binary(_) do
+    nil
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    type_got_from_binary = type_from_binary(file_binary)
+    type_got_from_extension = type_from_extension(extension)
+
+    cond do
+      type_got_from_binary === nil -> {:error, "Warning, file format and file extension do not match."}
+      type_got_from_binary === type_got_from_extension -> {:ok, type_got_from_extension}
+      true -> {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
