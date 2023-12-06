d0e29283f1ac0848f36a9f90b4dbdc5f75c46e8b
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2023-12-06 20:09:26 +0900

    feat: remote control car 첫번째 제출

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -2,29 +2,27 @@ defmodule RemoteControlCar do
   @enforce_keys [:nickname]
   defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new() do
-    %RemoteControlCar{nickname: "none"}
-  end
-  def new(nickname) do
+  def new(nickname \\ "none") do
     %RemoteControlCar{nickname: nickname}
   end
 
-  def display_distance(remote_car) do
-    %{RemoteControlCar.new() | nickname: "none"}
-    %{RemoteControlCar.new() | battery_percentage: "none"}
-    %{RemoteControlCar.new() | distance_driven_in_meters: "none"}
-
+  def display_distance(remote_car = %RemoteControlCar{}) do
     "#{remote_car.distance_driven_in_meters} meters"
   end
 
-  def display_battery(remote_car) when remote_car.battery_percentage === 0 do
+  def display_battery(remote_car = %RemoteControlCar{}) when remote_car.battery_percentage === 0 do
     "Battery empty"
   end
-  def display_battery(remote_car) do
+  def display_battery(remote_car = %RemoteControlCar{}) do
     "Battery at #{remote_car.battery_percentage}%"
   end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+  def drive(remote_car = %RemoteControlCar{}) when remote_car.battery_percentage >= 1 do
+    remote_car
+      |> Map.update!(:distance_driven_in_meters, fn v -> v + 20 end)
+      |> Map.update!(:battery_percentage, fn v -> v - 1 end)
+  end
+  def drive(remote_car = %RemoteControlCar{}) do
+    remote_car
   end
 end
